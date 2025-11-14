54e5690a835400a68b7940454b67906b6ee39021
Dennis Lehmann <wuestefelddennis@gmail.com>
2025-11-15 00:36:49 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <section id='navbar'>
+
+      <div>
+        <div>
+            <img 
+                src='/logo.svg'
+            />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Navbar
