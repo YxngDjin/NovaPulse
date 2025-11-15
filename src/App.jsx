a2773d4ae8a269ab523bbf84134dfc2cb1473b72
Dennis Lehmann <wuestefelddennis@gmail.com>
2025-11-15 16:16:11 +0100

    Add Video Section, Fix Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import VideoSection from './components/VideoSection'
 
 const App = () => {
   return (
     <main>
         <Navbar />
         <Hero />
+        <VideoSection />
     </main>
   )
 }
