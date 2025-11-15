0f468346a5f197865c8510ec74b50078d9733cca
Dennis Lehmann <wuestefelddennis@gmail.com>
2025-11-15 01:07:50 +0100

    Implement Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
+import navItems from '../constants'
 
 const Navbar = () => {
   return (
     <section id='navbar'>
 
-      <div>
+
+      <div className='flex w-full items-center justify-between'>
         <div>
             <img 
                 src='/logo.svg'
+                alt='logo'
+                className='w-10 h-10'
             />
+        </div>
+
+        <div className='hidden md:flex gap-5'>
+            {navItems.map(({id, name, link }) => (
+                <a 
+                    href={link}
+                    key={id}
+                >
+                    {name}
+                </a>
+            ))}
+        </div>
+
+        <div className='flex gap-5'>
+            <button>
+                <img 
+                    src='/icons/bag.svg'
+                    alt='card'
+                />
+            </button>
+            <button>
+                <img 
+                    src='/icons/search.svg'
+                    alt='search'
+                />
+            </button>
         </div>
       </div>
     </section>
---
+++ src/constants/index.js
@@ -0,0 +1,29 @@
+const navItems = [
+    {
+        id: 1,
+        name: 'Home',
+        link: '/home'
+    },
+    {
+        id: 2,
+        name: 'Store',
+        link: '/store'
+    },
+    {
+        id: 3,
+        name: 'Latest',
+        link: '/latest'
+    },
+    {
+        id: 4,
+        name: 'Samples',
+        link: '/samples'
+    },
+    {
+        id: 5,
+        name: 'Support',
+        link: '/support'
+    }
+]
+
+export default navItems
